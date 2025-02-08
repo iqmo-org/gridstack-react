e067cae87fcf1ecc67e05876951103cfafc18b41
CNine <cnine229@gmail.com>
2025-02-08 12:10:43 +0800

    chore: update lib exports

--- src/lib/grid-stack-container.tsx
+++ src/lib/grid-stack-container.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 import { GridStackProvider } from "./grid-stack-provider";
 import { GridStackRender } from "./grid-stack-render";
 
-type GridStackContainerProps = PropsWithChildren<{
+export type GridStackContainerProps = PropsWithChildren<{
   initialOptions: GridStackOptions;
   renderRawContent?: boolean;
 }>;
